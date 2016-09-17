cceb4b5ee0e9dc0a676c388c88875eaf7d1ceb88
Jack M <exegete@mac.com>
2016-09-16 20:29:28 -0700

    Added skip buttons, and made more navigable.

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -26,7 +26,24 @@ describe('LearnJS', function() {
   describe('problem view', function() {
     it('has a title that includes the problem number', function() {
       var view = learnjs.problemView('1');
-      expect(view.text()).toEqual('Problem #1 Coming soon!');
+      expect(view.text()).toContain('Problem #1');
+      var view = learnjs.problemView('69');
+      expect(view.text()).toContain('Problem #69');
     });
+  });
+});
+
+describe('answer section', function() {
+  it('can check a correct answer by hitting a button', function() {
+    var view = learnjs.problemView('1');
+    view.find('.answer').val('true');
+    view.find('.check-btn').click();
+    expect(view.find('.result').text()).toContain('Correct!');
+  });
+  it('can check a correct answer by hitting a button', function() {
+    var view = learnjs.problemView('1');
+    view.find('.answer').val('false');
+    view.find('.check-btn').click();
+    expect(view.find('.result').text()).toEqual('Incorrect!');
   });
 });
